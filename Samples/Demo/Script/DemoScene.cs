4582524f91ca9576497cee3a8c63cfba4557b036
RadBear <nbhung71711@gmail.com>
2024-09-08 18:53:40 +0700

    rcore-submodule

---
+++ Samples/Demo/Script/DemoScene.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace RCore.Demo
+{
+    public class DemoScene : MonoBehaviour
+    {
+
+    }
+}
---
+++ Samples/Demo/Script/ExampleScene.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace RCore.Demo
+{
+    public class ExampleScene : MonoBehaviour
+    {
+        void Start()
+        {
+            ExampleGameData.Instance.Init();
+            MainPanel.instance.Init();
+            ExamplePoolsManager.Instance.Init();
+        }
+    }
+}
